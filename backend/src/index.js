59e14770657f3cf831984e0a9f074d29a38dba22
kanza <kanza.shahid@teo-intl.com>
2025-08-19 16:01:01 +0500

    issue resolved

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,16 +28,16 @@ app.use(
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
 
 server.listen(PORT, () => {
   console.log("Server is running on port", PORT);
